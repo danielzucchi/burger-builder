caef3c921a9b0b712c73bfb6fbe10e2c8a6f2ada
Daniel Zucchi <danielzucchi@AND0128.local>
2019-01-03 14:12:00 +0000

    Add ingredient controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -16,7 +16,6 @@ const burger = props => {
     transformedIngredients = <p>Please add ingredients.</p>;
   }
 
-  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 1.5,
+  meat: 3.0,
+  bacon: 1.5
+};
 
 class BurgerBuilder extends Component {
   state = {
@@ -9,14 +17,35 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  componentDidUpdate() {
+    console.log(this.state.totalPrice);
+  }
+
+  removeIngredientHandler = type => {};
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <h3>Price: £{this.state.totalPrice}</h3>
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
