15e23f42ed87eb455742c71be3663b56b4a1c766
Daniel Zucchi <danielzucchi@AND0128.local>
2019-01-04 11:59:29 +0000

    Modal buttons

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,6 +2,8 @@ import React from "react";
 import classes from "./Backdrop.css";
 
 const backdrop = props =>
-  props.show ? <div class={classes.Backdrop} onClick={props.clicked} /> : null;
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null;
 
 export default backdrop;
